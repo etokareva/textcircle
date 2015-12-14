2889fcbe47bc32debc960a56cf0f19eb6c8461e9
etokareva <etokareva1990@gmail.com>
2015-12-14 18:34:53 +0300

    Live code editor

--- textcircle.js
+++ textcircle.js
@@ -1,7 +1,7 @@
 this.Documents = new Mongo.Collection("documents");
 
-
 if (Meteor.isClient) {
+
     Template.editor.helpers({
         docid:function(){
             var doc = Documents.findOne();
@@ -9,6 +9,17 @@ if (Meteor.isClient) {
                 return doc._id;
             } else {
                 return undefined;
+            }
+        },
+        config:function(){
+            return function(editor){
+                editor.setOption("lineNumbers", true);
+                editor.setOption("mode", "html");
+                editor.on("change", function(cm_editor, info){
+                    //console.log(cm_editor.getValue());
+                    $("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
+                });
+                console.log(editor);
             }
         }
     });
