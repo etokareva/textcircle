0c0bfed6d5f4800f476ad333344104c7ba719ab3
etokareva <etokareva1990@gmail.com>
2015-12-16 21:29:57 +0300

    Commit4

--- textcircle.js
+++ textcircle.js
@@ -1,4 +1,5 @@
 this.Documents = new Mongo.Collection("documents");
+EditingUsers = new Mongo.Collection("editingUsers");
 
 if (Meteor.isClient) {
 
@@ -18,12 +19,30 @@ if (Meteor.isClient) {
                 editor.on("change", function(cm_editor, info){
                     //console.log(cm_editor.getValue());
                     $("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
+                    Meteor.call("addEditingUser");
                 });
                 //console.log(editor);
             }
         }
     });
+
+    Template.editingUsers.helpers({
+        users:function(){
+            var doc, eusers, users;
+            doc = Documents.findOne();
+            if(!doc){ return; } //give up
+            eusers = EditingUsers.findOne({docid:doc._id});
+            if(!users){return;}
+            for (var user_id in eusers.users){
+                users[i] = eusers.user[user_id];
+                i++;
+            }
+
+            return users;
+        }
+    });
 }
+//end client
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
@@ -32,3 +51,20 @@ if (Meteor.isServer) {
     }
   });
 }
+
+Meteor.methods({
+    addEditingUser: function(){
+        var doc, user, eusers;
+        doc = Documents.findOne();
+        if(!doc){return;}
+        if(!this.userId){return;}
+        user = Meteor.user().profile;
+        eusers = EditingUsers.findOne({docid:doc._id});
+        if(!eusers){
+            eusers ={
+                docid:doc._id,
+            };
+        }
+        EditingUsers.upsert({_id:eusers._id}, eusers);
+    }
+});
